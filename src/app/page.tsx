b86a0c43ac0b1fe8138380637c5712776955e0ef
idabblewith <109533655+idabblewith@users.noreply.github.com>
2024-10-10 00:25:05 +0800

    chore: base setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+			<Button>Primary</Button>
+			<Button variant="secondary">Secondary</Button>
+			<Button variant="tertiary">Tertiary</Button>
+			<Button variant="destructive">Destructive</Button>
+			<Button variant="ghost">Ghost</Button>
+			<Button variant="muted">Muted</Button>
+			<Button variant="outline">Outline</Button>
+		</div>
+	);
+}
